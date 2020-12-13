32aad8278152f58752120178b62b930dbbaabd25
Philémon van Helden <philemon.vanhelden@gmx.com>
2020-12-13 13:24:07 +0100

    Grouped "free" lines inside "run" method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,6 @@
 import scala.collection.mutable.{ArrayBuffer, Map}
 
 object Main extends App {
-    val start = System.nanoTime()
-
     def parse_flights(path: String): ArrayBuffer[Array[String]] = {
         val content = new ArrayBuffer[Array[String]]()
         val file = io.Source.fromFile(path)
@@ -24,7 +22,7 @@ object Main extends App {
         content
     }
 
-    def question_1(): Array[Array[Int]] = {
+    def question_1(flights: ArrayBuffer[Array[String]]): Array[Array[Int]] = {
         val res = Array.ofDim[Int](12, 2)
         val flight_count = Array.fill(12)(0)
         for (flight <- flights) {
@@ -36,7 +34,7 @@ object Main extends App {
         res
     }
 
-    def question_2(): Array[Array[String]] = {
+    def question_2(flights: ArrayBuffer[Array[String]], passengers: Map[Int, Array[String]]): Array[Array[String]] = {
         val res = Array.ofDim[String](100, 4)
         val passenger_count = Array.fill(passengers.size)(0)
         for (flight <- flights) {
@@ -50,8 +48,8 @@ object Main extends App {
         res
     }
 
-    def question_3(): Array[Array[Int]] = {
-        val res = Array.ofDim[Int](passengers.size, 2)
+    def question_3(flights: ArrayBuffer[Array[String]], nbr_passengers: Int): Array[Array[Int]] = {
+        val res = Array.ofDim[Int](nbr_passengers, 2)
         val sorted_flights = flights.sortWith(_ (0).toInt < _ (0).toInt)
         var ids = Vector[Int]()
         for (flight <- sorted_flights) {
@@ -59,7 +57,7 @@ object Main extends App {
         }
         var current_index = 0
         var next_index = 0
-        for (id <- Range(1, passengers.size + 1, 1)) {
+        for (id <- Range(1, nbr_passengers + 1, 1)) {
             next_index = ids.indexOf(id + 1)
             if (next_index == -1) {
                 next_index = flights.size
@@ -88,33 +86,38 @@ object Main extends App {
         res
     }
 
-    val flights = parse_flights("Flight Data Assignment/flightData.csv")
-    val passengers = parse_passengers("Flight Data Assignment/passengers.csv")
+    def run(): Unit = {
+        val start = System.nanoTime()
+        val flights = parse_flights("Flight Data Assignment/flightData.csv")
+        val passengers = parse_passengers("Flight Data Assignment/passengers.csv")
 
-    println("Question 1:")
-    val res_q1 = question_1()
-    println("Month, Number of Flights")
-    for (month <- res_q1) {
-        println(month.mkString(", "))
+        println("Question 1:")
+        val res_q1 = question_1(flights)
+        println("Month, Number of Flights")
+        for (month <- res_q1) {
+            println(month.mkString(", "))
+        }
+        println()
+
+        println("Question 2:")
+        val res_q2 = question_2(flights, passengers)
+        println("Passenger ID, Number of Flights, First name, Last name")
+        for (passenger <- res_q2) {
+            println(passenger.mkString(", "))
+        }
+        println()
+
+        println("Question 3:")
+        val res_q3 = question_3(flights, passengers.size)
+        println("Passenger ID, Longest Run")
+        for (passenger <- res_q3) {
+            println(passenger.mkString(", "))
+        }
+        println()
+
+        var end = System.nanoTime()
+        println("Elapsed time: " + (end - start) / 1000000 + "ms")
     }
-    println()
 
-    println("Question 2:")
-    val res_q2 = question_2()
-    println("Passenger ID, Number of Flights, First name, Last name")
-    for (passenger <- res_q2) {
-        println(passenger.mkString(", "))
-    }
-    println()
-
-    println("Question 3:")
-    val res_q3 = question_3()
-    println("Passenger ID, Longest Run")
-    for (passenger <- res_q3) {
-        println(passenger.mkString(", "))
-    }
-    println()
-
-    var end = System.nanoTime()
-    println("Elapsed time: " + (end - start) / 1000000 + "ms")
+    run()
 }
